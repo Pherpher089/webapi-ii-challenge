80b023ac3f115a4beded0091ce4be7441a7e6436
Pherpher089 <Christutor089@gmail.com>
2019-07-26 13:23:53 -0700

    Added in the GET /api/posts/:id/comments, POST /api/posts/:id/comments and DELETE /api/posts/:id

--- routers/PostsRouter.js
+++ routers/PostsRouter.js
@@ -9,8 +9,82 @@ postRouter.get('/', (req, res) => {
         res.status(200).json(posts)
     })
     .catch(err => {
-        res.status(500).json(err)
+        res.status(500).json({err: "The posts information could not be retrieved."})
     })
 })
+
+postRouter.get('/:id', (req, res) => {
+
+    db.findById(req.params.id)
+    .then(posts =>{
+        if(posts === []) {
+            res.status(404).jason({message: `The post with id: ${req.params.id} does not exist`})
+        }
+        res.status(200).json(posts)
+    })
+    .catch(err => {
+        res.status(500).json({err: "The posts information could not be retrieved."})
+    })
+})
+
+postRouter.post('/', async (req, res) => {
+    const {title, contents} = req.body;
+    if(!title || !contents) {
+        res.status(400).json({errorMessage: "Please provide title and contents for the post."})
+    }
+    try {
+        const post = await db.insert(req.body)
+        res.status(201).json(post)
+    } catch (error) {
+        res.status(500).json({error: "There was an error while sving the post to the database"})
+    }
+})
+
+postRouter.post('/:id/comments', async (req, res) => {
+    if(!db.findById(req.params.id)) {
+        res.status(404).json({message: `The post with the id ${req.params.id} does not exist`})
+    }
+
+    if(!req.body.text)
+    {
+        res.status(400).json({errorMessage: "Please provide text for the comment"})
+    }
+    try {
+        const comment = await db.insertComment(req.body)
+        res.status(201).json(comment);
+    } catch (error) {
+        res.status(500).json({error: "There was an error while saving the comment to the database"})
+    }
+})
+
+postRouter.get('/:id/comments', async (req, res) => {
+    const id = req.params.id;
+    if(!db.findById(id)){
+        res.status(404).json({message: "The post with the specified ID does not exist"})
+    }
+
+    try{
+        const comment = await db.findPostComments(req.params.id)
+        res.status(200).json(comment)
+    } catch (error) {
+        res.status(500).json({errorMessage: "The comments information could not be retrieved."})
+    }
+})
+
+postRouter.delete('/:id', async (req, res) => {
+    if(!db.findById(req.params.id)) {
+        res.status(404).json({message: "The post with the specified ID does not exist"})
+    }
+
+    try {
+        const deletedPost = await db.remove(req.params.id)
+        res.status(200).json(deletedPost)
+    } catch (error) {
+        res.status(500).json({errorMessage: "There was an error while removing this post from the database"})        
+    }
+})
+
+
+
 
 module.exports = postRouter;
